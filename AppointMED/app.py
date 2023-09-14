db25c63a4dab225bdbfc2743105e4058a442f276
victorvegaward <victor.vega6@upr.edu>
2023-09-14 10:53:25 -0400

    db connect

--- AppointMED/app.py
+++ AppointMED/app.py
@@ -11,13 +11,11 @@ from event import Event
 app = Flask(__name__)
 
 # name of database
-app.config["MONGO_DBNAME"] = "AppointMED"
+app.config["MONGO_DBNAME"] = "PropertEase"
 
 # URI of database
 app.config[
-    'MONGO_URI'] = "mongodb+srv://admin:" + os.environ.get(
-    "MONGO_PW") + "@cluster0.vq3ym.mongodb.net/AppointMED?retryWrites=true&w" \
-                  "=majority"
+    'MONGO_URI'] = "mongodb+srv://PropertEase:PropertEase@cluster0.zc1kdsn.mongodb.net/PropertEase"
 
 time_slots = ["08:00am", "08:30am", "09:00am", "09:30am", "10:00am", "10:30am", "11:00am", "11:30am", "12:00pm",
               "12:30pm", "01:00pm", "01:30pm", "02:00pm", "02:30pm", "03:00pm", "03:30pm", "04:00pm", "04:30pm"]
